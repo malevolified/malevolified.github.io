e6c2713d3cbe49e380860a650a8795d25da2f4da
Malvo <malevolified@hotmail.com>
2021-04-03 19:12:58 -0300

    Lore
    -Added some lore to the KnownGuarians base tab

--- src/Pages/Guarians/Tabs/KnownGuarians/KnownGuarians.tsx
+++ src/Pages/Guarians/Tabs/KnownGuarians/KnownGuarians.tsx
@@ -26,7 +26,21 @@ const KnownGuarians: React.FC<IProps> = ({}) => {
         {guarian ? (
           <GuarianCharacterReference guarian={guarian} />
         ) : (
-          <div>Please select a guarian</div>
+          <div>
+            <h1>Known Guarians</h1>
+            <p>
+              Listed here are all of the known (or at least well documented) guarians. The actual
+              amount of guarians out there is truly unknown and possibly ever changing, and this
+              list may be extended as more are discovered.
+            </p>
+            <p>
+              As the guarians are often a highly dynamic race, the information provided for each is
+              purely subjective and written from the perspective of the author of these notes. In
+              reality, the guarians often adapt to those of who they are bound to and as such, one
+              guarian may appear or behave completely differently from one person to the next, only
+              keeping with them the very core of who they are.
+            </p>
+          </div>
         )}
       </div>
     );
@@ -34,7 +48,7 @@ const KnownGuarians: React.FC<IProps> = ({}) => {
 
   function renderLinks() {
     return (
-      <div style={{ width: 300, overflowY: "auto" }}>
+      <div className="character-ref" style={{ width: 300, overflowY: "auto" }}>
         {availableGuarians.map((g) => (
           <GuarianReferenceLink guarian={g} />
         ))}
