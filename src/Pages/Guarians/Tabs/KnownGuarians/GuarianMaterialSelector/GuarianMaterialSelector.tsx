2fe2904210af12b165d91f7f67b3419450f8254f
Malvo <malevolified@hotmail.com>
2021-04-09 11:49:19 -0300

    Material icons (and I don't mean react Material)
    -The material selector now has nicely styled icons

--- src/Pages/Guarians/Tabs/KnownGuarians/GuarianMaterialSelector/GuarianMaterialSelector.tsx
+++ src/Pages/Guarians/Tabs/KnownGuarians/GuarianMaterialSelector/GuarianMaterialSelector.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { GuarianMaterial } from "../images/guarianImagePack";
 import "./guarianMaterialSelector.css";
+import fluffyIcon from "./icons/fluffyIcon.png";
+import rubberIcon from "./icons/rubberIcon.png";
+import inflatableIcon from "./icons/inflatableIcon.png";
 
 interface IProps {
   materials: GuarianMaterial[];
@@ -9,6 +12,17 @@ interface IProps {
 }
 
 const GuarianMaterialSelector: React.FC<IProps> = ({ materials, selected, onSelect }) => {
+  function getIconForMaterial(material: GuarianMaterial) {
+    switch (material) {
+      case GuarianMaterial.Rubber:
+        return rubberIcon;
+      case GuarianMaterial.Inflatable:
+        return inflatableIcon;
+      default:
+        return fluffyIcon;
+    }
+  }
+
   return (
     <div className="material-selector-list" style={{ display: "flex" }}>
       {materials.map((m) => (
@@ -17,7 +31,7 @@ const GuarianMaterialSelector: React.FC<IProps> = ({ materials, selected, onSele
           className={"material-selector" + (m == selected ? " selected" : "")}
           onClick={() => onSelect(m)}
         >
-          Material
+          <img src={getIconForMaterial(m)} />
         </div>
       ))}
     </div>
