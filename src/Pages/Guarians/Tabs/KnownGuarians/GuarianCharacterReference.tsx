91b7b16aa2dc5b8a7615cb4227ce7ecd2a5f434d
Malvo <malevolified@hotmail.com>
2021-04-09 23:00:33 -0300

    Ref image behavior
    -The guarian image ref FINALLY behaves in at least an OKAY manner

--- src/Pages/Guarians/Tabs/KnownGuarians/GuarianCharacterReference.tsx
+++ src/Pages/Guarians/Tabs/KnownGuarians/GuarianCharacterReference.tsx
@@ -34,16 +34,20 @@ const GuarianCharacterReference: React.FC<IProps> = ({ guarian }) => {
           <span className="sub-title">{guarian.subTitle}</span>
         </h1>
         <div style={{ display: "flex" }}>
-          <div>
+          <div style={{ display: "flex", flexDirection: "column", justifyContent: "center" }}>
             <GuarianMaterialSelector
               materials={guarian.images.refs.map((r) => r.material)}
               selected={selectedMaterial}
               onSelect={setSelectedMaterial}
             />
-            <img
-              src={ref}
-              style={{ width: "calc(100vw - 1300px)", maxWidth: 900, minWidth: 400 }}
-            />
+            <div
+              style={{
+                width: "calc(100vw - 1138px)",
+                textAlign: "center",
+              }}
+            >
+              <img src={ref} style={{ maxWidth: "100%", maxHeight: "100%" }} />
+            </div>
           </div>
           <div>
             <div style={{ minHeight: 441, marginRight: 270 }}>{guarian.notes}</div>
