1fa4c6346014595d4821e48916e7b403b706b472
Malvo <malevolified@hotmail.com>
2021-04-25 11:50:32 -0300

    Lore expansion
    -Added lore for facelessness
    -Added nice styling for lore

--- src/Pages/Guarians/Tabs/Lore/Lore.tsx
+++ src/Pages/Guarians/Tabs/Lore/Lore.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import styles from "./lore.module.css";
 
 interface IProps {}
 
 const Lore: React.FC<IProps> = ({}) => {
-  return <div></div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.tidbit}>
+        <h3>Facial Features</h3>
+        <p>
+          One of the most notable and immediately apparent features of their guarians is their
+          typical lack of facial features. While total facelessness can be found in some guarians,
+          there are a few facial features that appear at varying frequencies. Ears are actually
+          relatively common across multiple varieties of guarians. Mouths are far rarer, but can
+          still be found on rare occassion. Not a single guarian with eyes has been seen to date
+          (not counting those who have had their eyes drawn on after the fact).
+        </p>
+      </div>
+    </div>
+  );
 };
 
 Lore.displayName = "Lore";
