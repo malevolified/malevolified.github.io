b422b9488f16e6cfc8c835d3470d5195a37d7214
Malvo <arastryx.dravoga@gmail.com>
2021-07-17 21:21:31 -0300

    WEREGUAR TRANSITIONS
    -The wereguar material transition is now based on CSS transitions instead of rapidly updated states and by god the math was not easy SLKDJFSDF

--- src/Pages/Guarians/Tabs/Lore/MaterialDisplay/MaterialDisplay.tsx
+++ src/Pages/Guarians/Tabs/Lore/MaterialDisplay/MaterialDisplay.tsx
@@ -4,51 +4,74 @@ import second from "./wereguarMaterials2.png";
 
 interface MaterialDisplayProps {}
 
-const images = [first, second];
+//THIS BREAKS IF YOU ONLY USE TWO IMAGES, you need to use three otherwise
+// the initial margin offset won't get set properly
+const images = [first, second, first, second];
 
 const IMAGE_WIDTH = 527;
 
 export const MaterialDisplay: React.FC<MaterialDisplayProps> = ({}) => {
   const [target, setTarget] = useState(0);
-  const [offset, setOffset] = useState(0);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      if (offset < IMAGE_WIDTH) {
-        setOffset(offset + 1);
-      } else {
-        setOffset(0);
-        setTarget(getNext());
-      }
-    }, 20);
+    setTarget(getNext());
+  }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setTarget(getNext()), 5000);
 
     return () => clearTimeout(timer);
-  }, [offset, target]);
+  }, [target]);
 
   function getNext() {
     return target + 1 >= images.length ? 0 : target + 1;
   }
 
+  function isActive(index: number) {
+    return index == target || index == getNext();
+  }
+
   function getCropWidth(index: number) {
     switch (index) {
       case target:
-        return IMAGE_WIDTH - offset;
+        return 0;
       case getNext():
-        return offset;
+        return IMAGE_WIDTH;
       default:
         return 0;
     }
   }
 
   function getSecondaryOffset(index: number) {
-    return index == getNext() ? offset - IMAGE_WIDTH : 0;
+    switch (index) {
+      case target:
+        return 0;
+      case getNext():
+        return 0;
+      default:
+        return -IMAGE_WIDTH;
+    }
   }
 
   return (
     <div style={{ display: "flex", flexDirection: getNext() == 0 ? "row-reverse" : "row" }}>
       {images.map((i, index) => (
-        <div key={i} style={{ maxHeight: 760, overflow: "hidden", width: getCropWidth(index) }}>
-          <img src={i} style={{ marginLeft: getSecondaryOffset(index) }} />
+        <div
+          key={i}
+          style={{
+            transition: isActive(index) ? "5s linear" : undefined,
+            maxHeight: 760,
+            overflow: "hidden",
+            width: getCropWidth(index),
+          }}
+        >
+          <img
+            src={i}
+            style={{
+              transition: isActive(index) ? "5s linear" : undefined,
+              marginLeft: getSecondaryOffset(index),
+            }}
+          />
         </div>
       ))}
     </div>
