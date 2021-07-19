5a3b94b59b8e11d92a7231c60e73a4850e2d612d
Malvo <arastryx.dravoga@gmail.com>
2021-07-18 22:11:57 -0300

    Footer
    -Added a site footer

---
+++ src/Pages/Guarians/GuarianFooter.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface GuarianFooterProps {}
+
+export const GuarianFooter: React.FC<GuarianFooterProps> = ({}) => {
+  return (
+    <div className="footer">
+      <div>© Malevolifed</div>
+      <div>PRE-RELEASE - July 18th, 2021</div>
+      <div>
+        <a href="http://twitter.com/malevolified" target="_blank">
+          <img width={30} height={30} src="/twitterLogo.png" />
+        </a>
+      </div>
+    </div>
+  );
+};
--- src/Pages/Guarians/GuarianRef.tsx
+++ src/Pages/Guarians/GuarianRef.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { NarrowScreen, WideScreen } from "../../Components/ScreenDetector";
+import { GuarianFooter } from "./GuarianFooter";
 import GuarianMenuRef from "./GuarianMenuRef";
 import "./guarianRef.css";
 import GuarianTabRef from "./GuarianTabRef";
@@ -9,13 +10,16 @@ interface IProps {}
 export const GuarianRef: React.FC<IProps> = ({}) => {
   return (
     <div className="guarian-ref">
-      <h1 className="title">The Guarians</h1>
-      <WideScreen>
-        <GuarianTabRef />
-      </WideScreen>
-      <NarrowScreen>
-        <GuarianMenuRef />
-      </NarrowScreen>
+      <div style={{ flex: 1, padding: "0 4vw" }}>
+        <h1 className="title">The Guarians</h1>
+        <WideScreen>
+          <GuarianTabRef />
+        </WideScreen>
+        <NarrowScreen>
+          <GuarianMenuRef />
+        </NarrowScreen>
+      </div>
+      <GuarianFooter />
     </div>
   );
 };
