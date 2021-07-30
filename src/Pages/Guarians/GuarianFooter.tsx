e22c97383b63e61bb12f6fce441fe0f0cde021f6
Malvo <arastryx.dravoga@gmail.com>
2021-07-29 21:15:32 -0300

    Update release
    -Oops

--- src/Pages/Guarians/GuarianFooter.tsx
+++ src/Pages/Guarians/GuarianFooter.tsx
@@ -6,7 +6,7 @@ export const GuarianFooter: React.FC<GuarianFooterProps> = ({}) => {
   return (
     <div className="footer">
       <div>© Malevolifed</div>
-      <div>PRE-RELEASE - July 18th, 2021</div>
+      <div>PRE-RELEASE - July 29th, 2021</div>
       <div>
         <a href="http://twitter.com/malevolified" target="_blank">
           <img width={30} height={30} src="/twitterLogo.png" />
