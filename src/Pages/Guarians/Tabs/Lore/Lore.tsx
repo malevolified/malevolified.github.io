555cab694125dbfeb2ba380f8cbadfd1d17fafea
Malvo <arastryx.dravoga@gmail.com>
2021-08-01 22:24:35 -0300

    ALL WRITING FINISHED
    -AAAAAAAAAAAAAAAAAAAAH
    -OVERVIEW IS WRITTEN
    -ALL WRITING IS DONE
    -Of course there are a few touch ups that are still needed here BUT THE BASELINE WRITING IS IMPLEMENTED

--- src/Pages/Guarians/Tabs/Lore/Lore.tsx
+++ src/Pages/Guarians/Tabs/Lore/Lore.tsx
@@ -15,6 +15,19 @@ const Lore: React.FC<IProps> = ({}) => {
       <Spirituality />
       <div style={{ paddingBottom: 50 }}></div>
       <Companionship />
+      <div style={{ paddingBottom: 50 }}></div>
+      <h1>That Which We Do Not Know</h1>
+      <p>
+        What makes the guarians truly fascinating is not just everything that's been documented
+        about them, but rather, everything that is not documented about them as well. For everything
+        we know about them, various counter examples of nearly every trait have been witnessed as
+        well. Some guarians seem to be capable of a great many things that we do not fully
+        understand, possessing intelligence far beyond what a guarian should, appearing in plain
+        site instead of only when you're alone, powers and abilities that we've never even seen. The
+        information given here is not exhaustive, and likely never will be. As long as there are
+        more guarians are encountered, we will never know all there is to know, and no one trait
+        will ever be guaranteed.
+      </p>
     </>
   );
 };
--- src/Pages/Guarians/Tabs/Overview/Overview.tsx
+++ src/Pages/Guarians/Tabs/Overview/Overview.tsx
@@ -1,11 +1,39 @@
 import React from "react";
+import styles from "./Overview.module.css";
 
 interface OverviewProps {}
 
 export const Overview: React.FC<OverviewProps> = ({}) => {
   return (
     <div>
-      <p>The guarians are very hard to explain lol</p>
+      <p className={styles.main}>
+        Unbound by the laws of nature and capable of a great many things, the guarians are a broad
+        collective of spiritual companions who simply wish to keep you company whenever you need
+        them most.
+      </p>
+      <p>
+        Rather unconventional by definition, the guarians could be referred to as 'corporeal
+        spirits'. They are neither dead nor alive, nor do they eat or even breathe, but they are not
+        simply spectres who can float about or pass through walls. The guarians are still very much
+        tangible and subject to at least some of the laws of physics when they're being perceived.
+        Outside of our perception, they suddenly become capable of incredible feats, disappearing or
+        appearing in an instant, changing size or even changing their very composition, but only
+        when you're not looking.
+      </p>
+      <p>
+        Despite their roles as spiritual guardians, they do not seem to be proficient protectors.
+        They don't seem to be highly intelligent nor are they very perceptive. They aren't very fast
+        and they tend to avoid conflict in the first place. Instead, the guarians seem to possess an
+        incredible level of emotional intelligence far in excess of what we ourselves are capable.
+        The guarians perceive, not the world around us, but rather, the world within us instead,
+        displaying an understanding of our very being beyond what we understand ourselves.
+      </p>
+      <p>
+        Though simplistic in nature, guarians of every shape, size and material have been
+        encountered, ever changing and ever shifting. Given their mysterious origins, their design
+        seem to know no limits, and as more are discovered, it seems as though the true extent of
+        their very existent remains unknown.
+      </p>
     </div>
   );
 };
