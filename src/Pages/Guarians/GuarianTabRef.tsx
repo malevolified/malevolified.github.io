b7d6eff7af1a38f8867b8e1437b61b51db424f54
Malvo <arastryx.dravoga@gmail.com>
2021-08-06 00:07:02 -0300

    Temp about button
    -Added an about button but I'm not sure about the placement so I'll probably remove it and pester people for help on where to place it

--- src/Pages/Guarians/GuarianTabRef.tsx
+++ src/Pages/Guarians/GuarianTabRef.tsx
@@ -1,3 +1,5 @@
+import { faQuestionCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { generatePath, NavLink } from "react-router-dom";
 import { GuarianRoutes } from "./GuarianRoutes";
@@ -8,19 +10,32 @@ interface IProps {}
 export const GuarianTabRef: React.FC<IProps> = ({}) => {
   return (
     <div>
-      <div className="tab-wrapper">
-        {refSections.map((s) => (
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <div className="tab-wrapper">
+          {refSections.map((s) => (
+            <NavLink
+              key={s.route}
+              exact={s.exact}
+              className="tab"
+              activeClassName="selected"
+              to={generatePath(s.route)}
+            >
+              {s.title}
+            </NavLink>
+          ))}
+        </div>
+        <div className="tab-wrapper">
           <NavLink
-            key={s.route}
-            exact={s.exact}
+            key={"/guarians/about"}
             className="tab"
             activeClassName="selected"
-            to={generatePath(s.route)}
+            to={"/guarians/about"}
           >
-            {s.title}
+            About
           </NavLink>
-        ))}
+        </div>
       </div>
+
       <div className="content flat-top">
         <GuarianRoutes />
       </div>
