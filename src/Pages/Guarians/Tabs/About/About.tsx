00d79e6ad291e0283d347c1d051cead338f09ca6
Malvo <arastryx.dravoga@gmail.com>
2021-08-06 14:16:59 -0300

    About Desc
    -Added a desc to the about page

--- src/Pages/Guarians/Tabs/About/About.tsx
+++ src/Pages/Guarians/Tabs/About/About.tsx
@@ -4,6 +4,8 @@ import { useScreenWidthGreaterThan } from "../../../../Hooks/useScreenWidthGreat
 import fluffy from "./fluffyMe.png";
 import squeaky from "./squeakyMe.png";
 import styles from "./About.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faQuoteLeft, faQuoteRight } from "@fortawesome/free-solid-svg-icons";
 
 interface AboutProps {}
 
@@ -19,7 +21,11 @@ export const About: React.FC<AboutProps> = ({}) => {
         <div style={{ textAlign: "center" }}>
           <h1
             className={styles.snugHeader}
-            style={isHorizontal ? { fontSize: "calc(4vw + 5px)" } : undefined}
+            style={
+              isHorizontal
+                ? { fontSize: "calc(4vw + 5px)", lineHeight: "calc(4vw + 5px)" }
+                : undefined
+            }
           >
             Malevolified
           </h1>
@@ -33,9 +39,36 @@ export const About: React.FC<AboutProps> = ({}) => {
             className={styles.snugHeader}
             style={isHorizontal ? { fontSize: "calc(0.9vw + 3px)" } : undefined}
           >
-            (Though they very well could be)
+            (Though he/they very well could be)
           </h6>
         </div>
+        <p style={{ fontSize: 21 }}>
+          <FontAwesomeIcon
+            icon={faQuoteLeft}
+            style={{ padding: "0px 15px", verticalAlign: "text-bottom" }}
+            size="lg"
+          />
+          Hello and welcome to <b>The Guarian Reference</b>! I am Malevolified or Malvo or Mal or
+          whatever really, and I am the creator and artist behind the guarians as well as the coder
+          of this very site! Admittedly, I'm not the best at writing descriptions for myself as I
+          like to let my creations speak louder than words (which is part of why this site is here
+          in the first place), but as you can probably tell, I have a profound appreciation for
+          friendly monsters and odd textures. I'll admit that, as you've probably gathered, I am
+          very biased towards rubber/inflatable forms for more reasons than I can fit in this
+          ideally short description of myself, but regardless of their make, I've always loved the
+          otherworldly, and I especially love the otherworldly that loves you back.
+        </p>
+        <p style={{ fontSize: 21 }}>
+          Regardless of what brought you here, I'm very flattered that you've ventured forth and
+          explored my creations. Having designed them as an open species, you're free to interpret
+          them or any of my creations as you will, and in the end, I sincerely hope that you get the
+          same joy from my monsters as I do.
+          <FontAwesomeIcon
+            icon={faQuoteRight}
+            style={{ padding: "0px 15px", verticalAlign: "text-top" }}
+            size="lg"
+          />
+        </p>
       </div>
     </div>
   );
