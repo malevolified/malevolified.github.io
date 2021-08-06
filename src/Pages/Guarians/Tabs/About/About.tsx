5c2e9c2bdd0658758513c135ef374b35689f2406
Malvo <arastryx.dravoga@gmail.com>
2021-08-06 13:31:47 -0300

    About headers
    -Added some cute headers to the about page

--- src/Pages/Guarians/Tabs/About/About.tsx
+++ src/Pages/Guarians/Tabs/About/About.tsx
@@ -1,14 +1,42 @@
 import React from "react";
 import ReactCompareImage from "react-compare-image";
+import { useScreenWidthGreaterThan } from "../../../../Hooks/useScreenWidthGreaterThan";
 import fluffy from "./fluffyMe.png";
 import squeaky from "./squeakyMe.png";
+import styles from "./About.module.css";
 
 interface AboutProps {}
 
 export const About: React.FC<AboutProps> = ({}) => {
+  const isHorizontal = useScreenWidthGreaterThan(800);
+
   return (
-    <div style={{ maxWidth: 794 }}>
-      <ReactCompareImage leftImage={fluffy} rightImage={squeaky} />
+    <div style={{ display: "flex", flexDirection: isHorizontal ? "row" : "column" }}>
+      <div style={{ maxWidth: 794, width: isHorizontal ? "calc(25% + 220px)" : "100%" }}>
+        <ReactCompareImage leftImage={fluffy} rightImage={squeaky} />
+      </div>
+      <div style={{ flex: 1 }}>
+        <div style={{ textAlign: "center" }}>
+          <h1
+            className={styles.snugHeader}
+            style={isHorizontal ? { fontSize: "calc(4vw + 5px)" } : undefined}
+          >
+            Malevolified
+          </h1>
+          <h4
+            className={styles.snugHeader}
+            style={isHorizontal ? { fontSize: "calc(1.6vw + 4px)" } : undefined}
+          >
+            Not actually a guarian
+          </h4>
+          <h6
+            className={styles.snugHeader}
+            style={isHorizontal ? { fontSize: "calc(0.9vw + 3px)" } : undefined}
+          >
+            (Though they very well could be)
+          </h6>
+        </div>
+      </div>
     </div>
   );
 };
